b0b4771a6fd0406bd1c93fa5b138dc80fee74250
Richard Boyd <55750710+rboyd85@users.noreply.github.com>
2020-04-18 15:48:08 +0100

    box collision

---
+++ Assets/Scripts/Player/BoxCollison.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject box;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+
+        {
+
+          
+            Destroy(box);
+        }
+
+    }
+}
